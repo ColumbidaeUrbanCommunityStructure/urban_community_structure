f55e2cc17b3927d8489d20662a4a1c393db432fb
James <james@Jamess-iMac.fritz.box>
2024-01-16 12:41:18 +0000

    Build communities for analysis

--- scripts/env.R
+++ scripts/env.R
@@ -27,22 +27,17 @@ TMP_DIR = '/tmp'
 
 WORKING_OUTPUT_DIR = mkdir('/Users/james/Projects', 'urban_community_structure_wrk')
 GEO_WORKING_OUTPUT_DIR = mkdir(WORKING_OUTPUT_DIR, 'geo')
-
 EBIRD_WORKING_OUTPUT_DIR = mkdir(WORKING_OUTPUT_DIR, 'ebird')
-
 BIRDLIFE_WORKING_OUTPUT_DIR = mkdir(WORKING_OUTPUT_DIR, 'birdlife')
 
 KEYS_DIR = mkdir(WORKING_OUTPUT_DIR, 'auth')
 
 DATA_OUTPUT_DIR = mkdir(mkdir('../../', 'data'), 'generated')
-GEO_DATA_OUTPUT_DIR = mkdir(DATA_OUTPUT_DIR, 'geo')
-
-EBIRD_DATA_OUTPUT_DIR = mkdir(DATA_OUTPUT_DIR, 'ebird')
-
+CITY_DATA_OUTPUT_DIR = mkdir(DATA_OUTPUT_DIR, 'city')
 TAXONOMY_OUTPUT_DIR = mkdir(DATA_OUTPUT_DIR, 'taxonomy')
-
 COMMUNITY_OUTPUT_DIR = mkdir(DATA_OUTPUT_DIR, 'community')
 
 PROVIDED_DATA = mkdir(mkdir('../..', 'data'), 'provided')
-
 PHYLO_TREE = filename(PROVIDED_DATA, 'phylogeny__stage2_hackett_mcc_no_neg.tre')
+
+FIGURES_OUTPUT_DIR = mkdir('../../', 'figures')
