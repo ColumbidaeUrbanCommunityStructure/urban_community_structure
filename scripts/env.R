00d926833141bfce96b99ef06f4ef7fb5a1e2cd1
James Richardson <jamese.richardson@autotrader.co.uk>
2024-09-17 12:54:52 +0100

    Add number of cities with introduced species per realm

--- scripts/env.R
+++ scripts/env.R
@@ -129,8 +129,8 @@ abundance_proxy_scale = scale_color_continuous(low = cyan, high = orange)
 # Third Party Data downloaded locally
 # ------------------------------------
 downloaded_data_file = function(file) {
-  paste('/Users/james/Dropbox/PhD/', file, sep = '')
-  # paste('/Users/jamese.richardson/Downloads/', file, sep = '')
+  # paste('/Users/james/Dropbox/PhD/', file, sep = '')
+  paste('/Users/jamese.richardson/Downloads/', file, sep = '')
 }
 
 # My mapping from Birdlife V8 to Jetz, this maps down the birdlife taxonomy versions to Birdlife V3 and thus Jetz. 
