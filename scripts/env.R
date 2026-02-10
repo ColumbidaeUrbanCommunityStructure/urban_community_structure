7c3451c9acf27ec348ecb8036ebf6cbde382886b
James Richardson <jamese.richardson@autotrader.co.uk>
2026-02-10 11:19:32 +0000

    Add analysis of normalised values alongside standardised values

--- scripts/env.R
+++ scripts/env.R
@@ -55,8 +55,8 @@ normalise = function(actual_value, max_value, min_value) {
   (actual_value - min_value) / (max_value - min_value)
 }
 
-test_value_wilcox = function(name, standarised_list) {
-  wilcox_test_result = wilcox.test(standarised_list, mu = 0, na.rm = T)
+test_value_wilcox = function(name, standarised_list, mu = 0) {
+  wilcox_test_result = wilcox.test(standarised_list, mu = mu, na.rm = T)
   
   significance = ifelse(wilcox_test_result$p.value < 0.0001, '***', 
                         ifelse(wilcox_test_result$p.value < 0.001, '**', 
@@ -135,8 +135,8 @@ abundance_proxy_scale_fill = scale_fill_continuous(low = cyan, high = orange)
 # Third Party Data downloaded locally
 # ------------------------------------
 downloaded_data_file = function(file) {
-  paste('/Users/james/Dropbox/PhD/', file, sep = '')
-  # paste('/Users/jamese.richardson/Downloads/', file, sep = '')
+  # paste('/Users/james/Dropbox/PhD/', file, sep = '')
+  paste('/Users/jamese.richardson/Downloads/', file, sep = '')
 }
 
 # My mapping from Birdlife V8 to Jetz, this maps down the birdlife taxonomy versions to Birdlife V3 and thus Jetz. 
